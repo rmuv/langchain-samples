08836aa2b9ce86a5873ce0cbe5cfde461d1e92f2
rmuvva <muvvaravikiran@gmail.com>
2026-03-27 13:01:49 -0700

    env setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv
+
+def main():
+    print("Hello from langchain-samples!")
+    print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
